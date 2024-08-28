573f6edfdfc5d8ca1f23119440bf26f0aba8ab9c
Eric Hernandez <derickhh.05@gmail.com>
2024-08-28 18:36:25 -0400

    made all necessary files and added basic code

---
+++ react-portfolio/src/components/Header.jsx
@@ -0,0 +1,12 @@
+// src/components/Header.jsx
+import React from 'react';
+
+const Header = () => {
+  return (
+    <header>
+      {/* Header content */}
+    </header>
+  );
+};
+
+export default Header; // Make sure this line is present
---
+++ react-portfolio/src/main.jsx
@@ -0,0 +1,7 @@
+// src/main.jsx
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import './style.css'; // Ensure global styles are applied
+
+ReactDOM.render(<App />, document.getElementById('root'));
